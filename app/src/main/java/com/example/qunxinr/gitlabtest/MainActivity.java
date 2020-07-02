016db6a55dd78e6a089b3a0dbe3e873ca81da345
linjujiu <linjujiu@gdqunxin.com>
2020-07-02 15:01:15 +0800

    第一次gitlab创建Android项目

--- app/src/main/java/com/example/qunxinr/gitlabtest/MainActivity.java
+++ app/src/main/java/com/example/qunxinr/gitlabtest/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.qunxinr.gitlabtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //快速生成全局变量   CTRL  alt  f
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.tv);
+
     }
 }
